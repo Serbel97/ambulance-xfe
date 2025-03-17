b763c3954cb79ecc926c45b5c98cd65b1d39cd32
Erik Belak <erik.belak@gmail.com>
2025-03-17 22:27:08 +0100

    unit tests

--- src/components/x-ambulance-wl-list/test/x-ambulance-wl-list.spec.tsx
+++ src/components/x-ambulance-wl-list/test/x-ambulance-wl-list.spec.tsx
@@ -7,12 +7,10 @@ describe('x-ambulance-wl-list', () => {
       components: [XAmbulanceWlList],
       html: `<x-ambulance-wl-list></x-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <x-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </x-ambulance-wl-list>
-    `);
+    const wlList = page.rootInstance as XAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+    expect(items.length).toEqual(expectedPatients);
   });
 });
